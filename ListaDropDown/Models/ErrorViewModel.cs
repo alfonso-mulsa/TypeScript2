ecf659b6e5c32026eb8c4a6e3f9ece3f122d3506
Alfonso Mulsa Fau <amulsa@gmail.com>
2024-04-03 10:37:17 +0200

    Agregar archivos de proyecto.

---
+++ ListaDropDown/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ListaDropDown.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
